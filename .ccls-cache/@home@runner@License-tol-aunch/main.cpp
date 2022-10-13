bea2f551350d179f351a1a5f282fc406b1456c24
Edwin Kurjak <112413421+elkurjak@users.noreply.github.com>
2022-10-13 15:19:42 +0000

    Went over it in class

--- .ccls-cache/@home@runner@License-tol-aunch/main.cpp
+++ .ccls-cache/@home@runner@License-tol-aunch/main.cpp
@@ -1,19 +1,46 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+//functions
+void printArray(const int list[], int listSize);
+int indexSmallestElement(const int list[], int listSize);
   
 int main()
 {
-    int n;
-    cin >>n;
-    const int ARRAY_SIZE = n;
-  
-    
-    for (int i = 0; i < ARRAY_SIZE; ++i){ 
-      
-     
-      
-      }
-  
-    return 0;
-  
+ int N;
+  cin >>N;
+
+  const int ARRAY_SIZE = N;
+  int myArray [ARRAY_SIZE];
+
+  for (int index = 0; index < ARRAY_SIZE; index++){
+
+    cin >> myArray[index];
   }
+
+ //printArray(myArray, ARRAY_SIZE);
+
+  int LaunchDay =  indexSmallestElement(myArray, ARRAY_SIZE);
+  cout <<LaunchDay;
+} 
+
+void printArray(const int list[], int listSize) {
+  int index;
+
+  for (index = 0; index < listSize; index++)
+    cout << list[index] << " ";
+
+  cout << endl;
+  return;
+}
+
+int indexSmallestElement(const int list[], int listSize) {
+  int minIndex = 0; // default assumption is to assume that the
+                    // largest value is in the first spot
+
+  for (int index = 1; index < listSize; index++) {
+    if (list[minIndex] > list[index])
+      minIndex = index;
+  }
+
+  return minIndex;
+}
